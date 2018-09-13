514064a962f79eae0bf6bad012117520879be382
Manny Shapir <mshapir95@gmail.com>
2018-09-13 19:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function logWhisper(string) { console.log(string.toLowerCase()) }
 function sayHiToGrandma(string) {
   
   if (string.toLowerCase()) { return "I can't hear you!" }
-
-} 
+  
+  } if (string.toUpperCase()) { return "YES INDEED!"}
