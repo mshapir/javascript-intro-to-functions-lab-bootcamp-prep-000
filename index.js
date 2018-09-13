961274581dd77ec19437a0281396167301be3e34
Manny Shapir <mshapir95@gmail.com>
2018-09-13 19:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function logShout(string) { console.log(string.toUpperCase()) }
 function logWhisper(string) { console.log(string.toLowerCase()) }
 
 function sayHiToGrandma(string) {
-  if()
+  if("")
 }
